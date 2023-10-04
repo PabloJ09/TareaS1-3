9ecf0b9a8e36eefb80f701f2d7468a27dd5da8b1
PabloJ09 <pablohera04@gmail.com>
2023-10-03 21:46:49 -0600

    TareaS_1-3 Enrega: semana 4

---
+++ src/main/java/com/TareaS1_3/TareaS13Application.java
@@ -0,0 +1,13 @@
+package com.TareaS1_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaS13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaS13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TareaS1_3/TareaS13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaS1_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaS13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
